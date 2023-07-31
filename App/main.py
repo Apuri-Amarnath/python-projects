bf47d13132facf9f6e028cd8d1aeb8026e118fdb
Amar-Lap <@Apuri-Amarnath>
2023-07-31 14:54:49 +0530

    buggy program

--- App/main.py
+++ App/main.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit, QMainWindow
 from PyQt6.QtGui import QFont
+
 class Calculator(QWidget):
 
     def __init__(self):
@@ -8,12 +9,13 @@ class Calculator(QWidget):
         self.ui()
 
     def ui(self):
+
         self.setWindowTitle("Calculator")
         self.buttons = [ ("7", "8", "9", "/"),
                          ("4", "5", "6", "*"),
                          ("1", "2", "3", "-"),
                          ("0", ".", "=", "+"),
-                         ("C", "CE")
+                         ("C", "CE","%")
                          ]
         self.layoutv = QVBoxLayout(self)
         self.input_txt_area = QLineEdit(self)
@@ -32,17 +34,53 @@ class Calculator(QWidget):
         self.setLayout(self.layoutv)
         self.input_txt_area.setReadOnly(True)
         self.input_txt_area.setFixedHeight(60)
-        self.input_txt_area.setFont(self.fontstyle)
+        self.font = self.input_txt_area.setFont(self.fontstyle)
+        self.temp_val = ""
+        self.result = None
 
     def Action(self):
         self.button = self.sender()
         if self.button:
             self.button_text = self.button.text()
-            self.input_txt_area.setText(self.button_text)
+            if self.button_text in "0123456789":
+                self.temp_val += self.button_text
+                self.input_txt_area.setText(self.temp_val)
+            elif self.button_text in "*+-/%":
+                self.operator_action()
+                self.temp_val = ""
+            elif self.button_text == "=":
+                if self.last_operator:
+                    self.operator_action()
+                    self.last_operator = None
+            elif self.button_text == "C":
+                self.input_txt_area.clear()
+                self.temp_val = ""
+            elif self.button_text == "CE":
+                self.input_txt_area.setText("")
+                self.temp_val = ""
+
+    def operator_action(self):
+        if self.result is None:
+            self.result = float(self.temp_val)
+        else:
+            operand = float(self.temp_val)
+            operator = self.input_txt_area.text()[-1]
+            if operator == '+':
+                self.result += operand
+            elif operator == '-':
+                self.result -= operand
+            elif operator == '*':
+                self.result *= operand
+            elif operator == '/':
+                self.result /= operand
+            elif operator == "%":
+                self.result %= operand
+
+        self.input_txt_area.setText(str(self.result))
 
 if __name__ == "__main__":
-        app = QApplication(sys.argv)
-        widget = Calculator()
-        widget.resize(500,500)
-        widget.show()
-        sys.exit(app.exec())
+    app = QApplication(sys.argv)
+    widget = Calculator()
+    widget.resize(500,500)
+    widget.show()
+    sys.exit(app.exec())
